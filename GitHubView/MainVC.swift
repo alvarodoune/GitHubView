432c9f94bc543e1ca21260559ef7bb680f9b5bdb
Alvaro Doune <alvarodoune@gmail.com>
2017-02-15 23:38:01 -0300

    Initial Commit

---
+++ GitHubView/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubView
+//
+//  Created by Alvaro Doune on 2/15/17.
+//  Copyright © 2017 Alvaro Doune. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
